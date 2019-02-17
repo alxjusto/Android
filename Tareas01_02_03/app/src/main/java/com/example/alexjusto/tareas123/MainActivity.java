da7e7ea0cecd25e6e2d6ac4d4be5d71d718e98e2
alxjusto <alex_justo@usmp.pe>
2019-02-17 12:16:12 -0500

    intentando realizar la segunda tarea

--- Tareas01_02_03/app/src/main/java/com/example/alexjusto/tareas123/MainActivity.java
+++ Tareas01_02_03/app/src/main/java/com/example/alexjusto/tareas123/MainActivity.java
@@ -2,12 +2,35 @@ package com.example.alexjusto.tareas123;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    List<Tarea> task;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Tarea tarea1 = new Tarea("tarea1", "21/05/2019", "2pm");
+        task.add(tarea1);
+
+        Tarea tarea2 = new Tarea("tarea2", "21/05/2019", "2pm");
+        task.add(tarea2);
+
+        Tarea tarea3 = new Tarea("tarea3", "21/05/2019", "2pm");
+        task.add(tarea3);
+
+        imprimir(task);
     }
+
+    public void imprimir(List<Tarea> tasks) {
+        for(Tarea lista: tasks){
+            Log.d(TAG,"Tareas: " + lista.getNombre());
+        }
+    }
+
 }
---
+++ Tareas01_02_03/app/src/main/java/com/example/alexjusto/tareas123/Tarea.java
@@ -0,0 +1,38 @@
+package com.example.alexjusto.tareas123;
+
+public class Tarea {
+
+    private String nombre;
+    private String fecha;
+    private String hora;
+
+    public Tarea(String nombre, String fecha, String hora) {
+        this.nombre = nombre;
+        this.fecha = fecha;
+        this.hora = hora;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getHora() {
+        return hora;
+    }
+
+    public void setHora(String hora) {
+        this.hora = hora;
+    }
+}
