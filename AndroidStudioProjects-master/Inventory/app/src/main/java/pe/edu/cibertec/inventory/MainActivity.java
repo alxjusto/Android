eada84a8bed29c59bb4d01f1ac31e5687edff887
alxjusto <alex_justo@usmp.pe>
2019-03-03 13:21:36 -0500

    new inventory project added

--- AndroidStudioProjects-master/Inventory/app/src/main/java/pe/edu/cibertec/inventory/MainActivity.java
+++ AndroidStudioProjects-master/Inventory/app/src/main/java/pe/edu/cibertec/inventory/MainActivity.java
@@ -1,8 +1,15 @@
 package pe.edu.cibertec.inventory;
 
+import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import java.util.ArrayList;
 
@@ -16,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
 
     // Adaptador
     AdapterProduct adapterProduct;
+
+    final static int REQUEST_CODE_MAIN = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,12 +42,18 @@ public class MainActivity extends AppCompatActivity {
 
         // Inicaliza el adaptador
         adapterProduct = new AdapterProduct(items);
+
+        rvProduct.setAdapter(adapterProduct);
+
+        rvProduct.setLayoutManager(new LinearLayoutManager(this));
+
+
     }
 
     private void loadItems() {
         // Instancia un objeto de la clase Product
         Product productLaptop = new Product("Laptop",
-                        "Marca Toshiba", 2);
+                "Marca Toshiba", 2);
 
         // Agrega el objeto creado a la lista
         items.add(productLaptop);
@@ -46,8 +61,37 @@ public class MainActivity extends AppCompatActivity {
         Product productMouse = new Product("Mouse",
                 "Modelo Genious", 122);
         items.add(productMouse);
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent intent = new Intent(this, ProductActivity.class);
+        startActivityForResult(intent, REQUEST_CODE_MAIN);
+
+        return true;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == REQUEST_CODE_MAIN && resultCode == RESULT_OK) {
+            String name = data.getStringExtra("product_name");
+            String description = data.getStringExtra("product_description");
+            int quantity = data.getIntExtra("product_quantity",0);
+
+            Product product = new Product(name, description, quantity);
+
+            items.add(product);
+
+            adapterProduct.notifyDataSetChanged();
+
+        }
 
     }
 }
